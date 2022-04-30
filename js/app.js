dd1b109a6adac2a4af58bff7fa85b3ed4d36e80a
Jonatan <jony@gmail.com>
2022-04-29 23:37:42 -0500

    viernes

---
+++ js/app.js
@@ -0,0 +1,21 @@
+//constructores
+function Seguro(marca, year, tipo){
+    this.marca = marca;
+    this.year = year;
+    this.tipo = tipo;
+}
+
+//Llena las opciones de los years
+UI.prototype.llenarOpciones = () => {
+    const max = new Date().getFullYear(),
+          min = max - 20;
+
+    const selectYear = document.querySelector('#year');
+
+    for(let i = max; i > min; i--){
+        let option = document.createElement('option');
+        option.value = i;
+        option.textContent = i;
+        selectYear.appendChild(option);
+    }
+}
